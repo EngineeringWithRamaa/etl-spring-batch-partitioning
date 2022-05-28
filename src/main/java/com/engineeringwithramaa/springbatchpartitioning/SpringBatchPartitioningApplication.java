68c2d3d7eaeecac639dfb8dfcf26c5801b16dcda
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-28 13:46:51 +0530

    Batch Processig triggered with Scheduler

--- src/main/java/com/engineeringwithramaa/springbatchpartitioning/SpringBatchPartitioningApplication.java
+++ src/main/java/com/engineeringwithramaa/springbatchpartitioning/SpringBatchPartitioningApplication.java
@@ -2,8 +2,10 @@ package com.engineeringwithramaa.springbatchpartitioning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBatchPartitioningApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/engineeringwithramaa/springbatchpartitioning/controller/LibraryRecordController.java
+++ src/main/java/com/engineeringwithramaa/springbatchpartitioning/controller/LibraryRecordController.java
@@ -8,14 +8,13 @@ import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteExcep
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("/etl/partition-batch")
@@ -25,6 +24,17 @@ public class LibraryRecordController {
     @Autowired
     private Job job;
 
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+
+    @Scheduled(cron = "0 38 13 28 * *")
+    public void scheduleByFixedRate() throws Exception {
+        System.out.println("***** BATCH PROCESSING STARTS *****");
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("time", format.format(Calendar.getInstance().getTime())).toJobParameters();
+        jobLauncher.run(job, jobParameters);
+        System.out.println("***** EXECUTION OVER ******\n");
+    }
+    /*
     @GetMapping
     public ResponseEntity<UserResponseDTO> startBatchPartitioning() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         Map<String, JobParameter> maps = new HashMap<>();
@@ -57,4 +67,5 @@ public class LibraryRecordController {
 
         return ResponseEntity.ok(userResponse);
     }
+    */
 }
